4947a5b9602a58151534c34e12c45f3d4ff5890e
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-02-14 07:11:49 +1100

    Add example Emacs Lisp file

---
+++ examples/example.el
@@ -0,0 +1,13 @@
+;;; -*- coding: utf-8; lexical-binding: t -*-
+;;; Author: ywatanabe
+;;; Timestamp: <2025-02-14 07:05:51>
+;;; File: /home/ywatanabe/.emacs.d/lisp/emacs-header-footer/examples/example.el
+(FILE CONTENTS HERE)
+
+(provide 'example)
+
+(when
+    (not load-file-name)
+  (message "example.el loaded."
+           (file-name-nondirectory
+            (or load-file-name buffer-file-name))))
